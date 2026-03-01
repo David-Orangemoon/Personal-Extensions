25bd81f1c37a7fc9fcb4836838691db522fa7cfe
Dr. Cleve <76855369+David-Orangemoon@users.noreply.github.com>
2026-02-28 20:36:52 -0500

    Make page.

---
+++ site.js
@@ -0,0 +1,78 @@
+const createExtensionElement = (extension) => {
+    if (typeof extension != "object") {
+        const p = document.createElement("h2");
+        p.className = "centerText"
+        p.innerText = extension;
+        return p;
+    }
+    const container = document.createElement("div");
+    switch (extension.type) {
+        case "category": {
+            container.className = "listCategory";
+            
+            const p = document.createElement("h1");
+            p.innerText = extension.name
+            p.className = "centerText";
+
+            container.appendChild(p);
+
+            parseList(container, extension.contents);
+            break;
+        }
+    
+        default: {
+            container.className = "extension";
+
+            const name = document.createElement("h3");
+            name.className = "centerText";
+            name.innerText = extension.name;
+
+            const description = document.createElement("p");
+            description.className = "centerText";
+            fetch(`extensions/${extension.id}/description.txt`).then(res => res.text()).then((text) => {
+                description.innerText = text;
+            }).catch(() => {});
+
+            const buttonHolder = document.createElement("div");
+            buttonHolder.className = "extensionButtons"
+
+            const copyCode = document.createElement("button");
+            copyCode.innerText = "Copy Code";
+            copyCode.style.setProperty("--color-1", "#2c61d4");
+            copyCode.style.setProperty("--color-2", "#1d50be");
+            
+            const copyURL = document.createElement("button");
+            copyURL.innerText = "Copy URL";
+            copyURL.style.setProperty("--color-1", "#2c88d4");
+            copyURL.style.setProperty("--color-2", "#146ab1");
+            
+            const openInMist = document.createElement("button");
+            openInMist.innerText = "Open in Mist-Warp";
+            openInMist.style.setProperty("--color-1", "#c02cd4");
+            openInMist.style.setProperty("--color-2", "#9d10af");
+
+            buttonHolder.appendChild(copyCode);
+            buttonHolder.appendChild(copyURL);
+            buttonHolder.appendChild(openInMist);
+
+            container.appendChild(name);
+            container.appendChild(description);
+            container.appendChild(buttonHolder);
+            break;
+        }
+    }
+
+    return container;
+}
+
+const parseList = (element, json) => {
+    for (let extensionID in json) {
+        const extension = json[extensionID];
+        element.appendChild(createExtensionElement(extension));
+    }
+}
+
+fetch("extensions/list.json").then(res => res.text()).then((text) => {
+    const json = JSON.parse(text);
+    parseList(document.body, json);
+})
